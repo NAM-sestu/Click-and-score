77d00373e3054b1933e7e7aca00b41f872371fc4
NAM-sestu <66006043+NAM-sestu@users.noreply.github.com>
2020-11-17 10:15:21 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace fta3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(127, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(203, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(47, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 37);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(242, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 37);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(242, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 37);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Brown;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(151, 435);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(66, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(242, 230);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(66, 37);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(242, 167);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(66, 37);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Red;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(242, 107);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(66, 37);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Red;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(47, 359);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(66, 37);
+            this.button8.TabIndex = 9;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Red;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(47, 298);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(66, 37);
+            this.button9.TabIndex = 10;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Red;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Location = new System.Drawing.Point(47, 230);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(66, 37);
+            this.button10.TabIndex = 11;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Red;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Location = new System.Drawing.Point(47, 167);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(66, 37);
+            this.button11.TabIndex = 12;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.Button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(394, 496);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fta3
+{
+    public partial class Form1 : Form
+    {
+        Timer t;
+        bool b1 = false, b2 = false, b3 = false, b4 = false, b5 = false, b6 = false, b7 = false, b8 = false, b9 = false, b10 = false;
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            t = new Timer();
+            t.Interval = 1010;
+            t.Tick += t_Tick;
+            t.Enabled = true;
+        }
+
+       
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            int i = rd.Next(0,9);
+            if (i == 0)
+            {
+                button1.BackColor = Color.Green;
+                b1 = true;
+                button2.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+                button5.BackColor = Color.Red;
+                button6.BackColor = Color.Red;
+                button7.BackColor = Color.Red;
+                button8.BackColor = Color.Red;
+                button9.BackColor = Color.Red;
+                button10.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b2 = false;
+                b3 = false;
+                b4 = false;
+                b5 = false;
+                b6 = false;
+                b7 = false;
+                b8 = false;
+                b9 = false;
+                b10 = false;
+            }
+            else if (i == 1)
+            {
+                button2.BackColor = Color.Green;
+                b2 = true;
+                button1.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+                button5.BackColor = Color.Red;
+                button6.BackColor = Color.Red;
+                button7.BackColor = Color.Red;
+                button8.BackColor = Color.Red;
+                button9.BackColor = Color.Red;
+                button10.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b1 = false;
+                b3 = false;
+                b4 = false;
+                b5 = false;
+                b6 = false;
+                b7 = false;
+                b8 = false;
+                b9 = false;
+                b10 = false;
+            }
+            else if (i == 2)
+            {
+                button3.BackColor = Color.Green;
+                b3 = true;
+                button2.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button5.BackColor = Color.Red;
+                button6.BackColor = Color.Red;
+                button7.BackColor = Color.Red;
+                button8.BackColor = Color.Red;
+                button9.BackColor = Color.Red;
+                button10.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b1 = false;
+                b2 = false;
+                b4 = false;
+                b5 = false;
+                b6 = false;
+                b7 = false;
+                b8 = false;
+                b9 = false;
+                b10 = false;
+            }
+            else if (i == 3)
+            {
+                button5.BackColor = Color.Green;
+                b4 = true;
+                button2.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button6.BackColor = Color.Red;
+                button7.BackColor = Color.Red;
+                button8.BackColor = Color.Red;
+                button9.BackColor = Color.Red;
+                button10.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b1 = false;
+                b3 = false;
+                b2 = false;
+                b5 = false;
+                b6 = false;
+                b7 = false;
+                b8 = false;
+                b9 = false;
+                b10 = false;
+            }
+            else if (i == 4)
+            {
+                button6.BackColor = Color.Green;
+                b5 = true;
+                button2.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+                button5.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button7.BackColor = Color.Red;
+                button8.BackColor = Color.Red;
+                button9.BackColor = Color.Red;
+                button10.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b1 = false;
+                b3 = false;
+                b4 = false;
+                b2 = false;
+                b6 = false;
+                b7 = false;
+                b8 = false;
+                b9 = false;
+                b10 = false;
+            }
+            else if (i == 5)
+            {
+                button7.BackColor = Color.Green;
+                b6 = true;
+                button2.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+                button5.BackColor = Color.Red;
+                button6.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button8.BackColor = Color.Red;
+                button9.BackColor = Color.Red;
+                button10.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b1 = false;
+                b3 = false;
+                b4 = false;
+                b5 = false;
+                b2 = false;
+                b7 = false;
+                b8 = false;
+                b9 = false;
+                b10 = false;
+            }
+            else if (i == 6)
+            {
+                button8.BackColor = Color.Green;
+                b7 = true;
+                button2.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+                button5.BackColor = Color.Red;
+                button6.BackColor = Color.Red;
+                button7.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button9.BackColor = Color.Red;
+                button10.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b1 = false;
+                b3 = false;
+                b4 = false;
+                b5 = false;
+                b6 = false;
+                b2 = false;
+                b8 = false;
+                b9 = false;
+                b10 = false;
+            }
+            else if (i == 7)
+            {
+                button9.BackColor = Color.Green;
+                b8 = true;
+                button2.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+                button5.BackColor = Color.Red;
+                button6.BackColor = Color.Red;
+                button7.BackColor = Color.Red;
+                button8.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button10.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b1 = false;
+                b3 = false;
+                b4 = false;
+                b5 = false;
+                b6 = false;
+                b7 = false;
+                b2 = false;
+                b9 = false;
+                b10 = false;
+            }
+            else if (i == 8)
+            {
+                button10.BackColor = Color.Green;
+                b9 = true;
+                button2.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+                button5.BackColor = Color.Red;
+                button6.BackColor = Color.Red;
+                button7.BackColor = Color.Red;
+                button8.BackColor = Color.Red;
+                button9.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b1 = false;
+                b3 = false;
+                b4 = false;
+                b5 = false;
+                b6 = false;
+                b7 = false;
+                b8 = false;
+                b2 = false;
+                b10 = false;
+            }
+              else if (i == 9)
+            {
+                button11.BackColor = Color.Green;
+                b10 = true;
+                button2.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+                button5.BackColor = Color.Red;
+                button6.BackColor = Color.Red;
+                button7.BackColor = Color.Red;
+                button8.BackColor = Color.Red;
+                button1.BackColor = Color.Red;
+                button10.BackColor = Color.Red;
+                button11.BackColor = Color.Red;
+                b1 = false;
+                b3 = false;
+                b4 = false;
+                b5 = false;
+                b6 = false;
+                b7 = false;
+                b8 = false;
+                b9 = false;
+                b2 = false;
+            }
+            else
+            {
+              }
+            
+            
+            
+            
+        }
+
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (b1==true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+               
+
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            if (b6 == true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+             
+
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            if (b10 == true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+               
+              
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            if (b5 == true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+            
+
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            if (b9 == true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+              
+
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if (b4 == true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+            
+
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            if (b8 == true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+               
+
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (b3 == true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+             
+
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            if (b7 == true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+            
+
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (b2 == true)
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc + 10;
+                label2.Text = sc.ToString();
+               
+            }
+            else
+            {
+                int sc = Convert.ToInt32(label2.Text);
+                sc = sc - 10;
+                label2.Text = sc.ToString();
+            }
+        }
+
+
+    }
+}
